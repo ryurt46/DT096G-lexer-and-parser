5365096ca70c2cf769d0c3d9932c804f97e0f94b
robinho46 <robin.yurt@hotmail.com>
2024-07-08 23:36:20 +0200

    feat: implemented the lexer according to my current grammar, fixes #1

--- lexer.cpp
+++ lexer.cpp
@@ -1,5 +1,66 @@
 #include "lexer.h"
+#include <cctype>
 
-Token Lexer::lexing(it& first, it last){
-    
+Token Lexer::lexer(it& first, it last){
+   while(std::isspace(*first) && first != last){
+        first++;
+    }
+    if(first == last){
+        return Token::END;
+    }
+    if(std::isdigit(*first)){
+        ++first;
+        return Token::DIGIT;
+    }
+    if(std::isalpha(*first)){
+        ++first;
+        return Token::CHAR;
+    }
+    switch (*first) {
+        case '+':
+            ++first;
+            return Token::OR;
+        case '*':
+            ++first;
+            return Token::GREEDY;
+        case '.':
+            ++first;
+            return Token::ANY;
+        case '(':
+            ++first;
+            return Token::LPAREN;
+        case ')':
+            ++first;
+            return Token::RPAREN;
+        case '{':
+            ++first;
+            return Token::LBRACE;
+        case '}':
+            ++first;
+            return Token::RBRACE;
+        case '\\':
+            ++first;
+            if (first != last) {
+                if (*first == 'I') {
+                    ++first;
+                    return Token::IGNORE;
+                } else if (*first == 'O') {
+                    ++first;
+                    if (first != last && *first == '{') {
+                        ++first;
+                        while (first != last && std::isdigit(*first)) {
+                            ++first;
+                        }
+                        if (first != last && *first == '}') {
+                            ++first;
+                            return Token::OUTPUT;
+                        }
+                    }
+                }
+            }
+            return Token::INVALID;
+        default:
+            ++first;
+            return Token::INVALID;
+    } 
 }
--- lexer.h
+++ lexer.h
@@ -5,25 +5,13 @@
 using it = std::string::iterator;
 
 enum Token {
-    OPEN_GROUP,      // '('
-    CLOSE_GROUP,     // ')'
-    OR,              // '+'
-    REPEAT,          // '*'
-    ANY_CHAR,        // '.'
-    OPEN_COUNT,      // '{'
-    CLOSE_COUNT,     // '}'
-    IGNORE_CASE,     // '\I'
-    OUTPUT_GROUP,    // '\O'
-    NUMBER,          // A sequence of digits
-    WORD,            // A sequence of characters that forms a word
-    END_OF_FILE,     // Special token to represent the end of the input
-    INVALID          // Special token to represent an invalid or unrecognized token
+    LPAREN, RPAREN, OR, GREEDY, ANY, LBRACE, RBRACE, IGNORE, OUTPUT, DIGIT, CHAR, END, INVALID
 };
 
 class Lexer{
 public:
     Lexer() = default;
-    Token lexing(it& first, it last);
+    Token lexer(it& first, it last);
 };
 
 #endif
